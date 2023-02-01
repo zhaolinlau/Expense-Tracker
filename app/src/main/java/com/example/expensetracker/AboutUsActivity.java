b9f8363dd0ee110e2061608339d50fe0a57882f6
CHAN YUEN FU <alsonchan332@gmail.com>
2023-02-01 17:57:12 +0800

    update about us

--- app/src/main/java/com/example/expensetracker/AboutUsActivity.java
+++ app/src/main/java/com/example/expensetracker/AboutUsActivity.java
@@ -3,15 +3,18 @@ package com.example.expensetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.expensetracker.databinding.ActivityAboutUsBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class AboutUsActivity extends AppCompatActivity {
     ActivityAboutUsBinding binding;
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+    private ImageView appLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,9 @@ public class AboutUsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         firebaseAuth=FirebaseAuth.getInstance();
         firebaseUser=firebaseAuth.getCurrentUser();
+
+        appLogo = findViewById(R.id.iv_appLogo);
+        Picasso.get().load("https://icons.iconarchive.com/icons/flat-icons.com/flat/256/Wallet-icon.png").into(appLogo);
+
     }
 }
