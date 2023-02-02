6b169b1d567620e9bd4fe191161f128defe67377
nabil <deblackdevil0421@gmail.com>
2023-02-02 19:53:07 +0800

    update about us

--- app/src/main/java/com/example/expensetracker/ReportActivity.java
+++ app/src/main/java/com/example/expensetracker/ReportActivity.java
@@ -1,9 +1,18 @@
 package com.example.expensetracker;
 
+import static android.hardware.Sensor.TYPE_LIGHT;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.expensetracker.databinding.ActivityReportBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,6 +30,10 @@ public class ReportActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
     FirebaseFirestore firebaseFirestore;
+
+
+
+
     int accountBalance=0;
     int sumExpense=0;
     int sumIncome=0;
@@ -32,6 +45,14 @@ public class ReportActivity extends AppCompatActivity {
     ArrayList<TransactionModel> transactionModelArrayList;
     TransactionAdapter transactionAdapter;
 
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightEventListener;
+    private View root;
+    private float maxValue;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +62,32 @@ public class ReportActivity extends AppCompatActivity {
         firebaseUser=firebaseAuth.getCurrentUser();
         firebaseFirestore=FirebaseFirestore.getInstance();
         transactionModelArrayList=new ArrayList<>();
+
+        root = findViewById(R.id.report);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
+
+        if (lightSensor == null){
+            Toast.makeText(this, "The device has no light sensor :(", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        maxValue = lightSensor.getMaximumRange();
+
+        lightEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float value = sensorEvent.values[0];
+                getSupportActionBar().setTitle("Luminosity : " + value + "lx");
+                int newValue = (int) (255f * value / maxValue);
+                root.setBackgroundColor(Color.rgb(newValue, newValue, newValue));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
     }
 
     protected void onStart() {
@@ -87,5 +134,14 @@ public class ReportActivity extends AppCompatActivity {
                         transactionAdapter=new TransactionAdapter(ReportActivity.this,transactionModelArrayList);
                     }
                 });
+    }
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(lightEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(lightEventListener);
     }
 }
