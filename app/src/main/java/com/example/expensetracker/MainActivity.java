8ad9b2090ef5beab5a60157d6afe1908385a0983
nabil <deblackdevil0421@gmail.com>
2023-02-02 18:39:40 +0800

    update about us

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -36,11 +36,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     WifiManager wifiManager;
 
-    private SensorManager sensorManager;
-    private Sensor lightSensor;
-    private SensorEventListener lightEventListener;
-    private View root;
-    private float maxValue;
+
 
 
 
@@ -54,32 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-        root = findViewById(R.id.root);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
-
-        if (lightSensor == null){
-            Toast.makeText(this, "The device has no light sensor :(", Toast.LENGTH_SHORT).show();
-            finish();
-        }
-
-        maxValue = lightSensor.getMaximumRange();
-
-        lightEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                float value = sensorEvent.values[0];
-                getSupportActionBar().setTitle("Luminosity : " + value + "lx");
-                int newValue = (int) (255f * value / maxValue);
-                root.setBackgroundColor(Color.rgb(newValue, newValue, newValue));
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
 
 
 
@@ -152,15 +122,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(lightEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
-    }
 
-    protected void onPause(){
-        super.onPause();
-        sensorManager.unregisterListener(lightEventListener);
-    }
 
 
 }
